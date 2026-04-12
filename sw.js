57412ae2b2ba60df248d1fbdccc307075a7a29a5
eairw13melo-glitch <eairw13melo@gmail.com>
2026-04-12 20:19:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,24 @@
-const CACHE_NAME = 'budget-app-v1';
-const ASSETS = [
-  '/or-amento/',
-  '/or-amento/index.html',
-  '/or-amento/style.css',
-  '/or-amento/script.js',
-  '/or-amento/manifest.json',
-  '/or-amento/favicon.svg'
+const CACHE_NAME = 'budget-app-v2';
+const STATIC_ASSETS = [
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/manifest.json',
+    '/favicon.svg'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {}));
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(STATIC_ASSETS.map(asset => new Request(asset, { mode: 'no-cors' })));
+    }).catch(() => {}));
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request).catch(() => {})));
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request).catch(() => new Response('Offline - Orçamento Familiar', { status: 503, statusText: 'Offline' })))
+    );
 });
